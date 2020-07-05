8bac1114e30245df82273eddf1d410350d876d9d
Bünyamin Öztürk <lanson2332@hotmail.com>
2020-07-05 06:44:22 +0300

    hotfix

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var os = require('os')
 setInterval(()=>{
 
     test.sendstatus();
-    console.log('status Sending to website HOSTNAME: '+ os.hostname()+' LINK: https://sapphire-magenta-wealth.glitch.me/pages/'+os.hostname())
+    console.log('status Sending to website HOSTNAME: '+ os.hostname()+' LINK: https://statuspage-ls.glitch.me/pages/'+os.hostname())
 
 
 },15000);
